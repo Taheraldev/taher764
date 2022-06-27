1ff04653310ea60f5e450b8f137ea920a6c4d7c5
Taheraldev <101641519+Taheraldev@users.noreply.github.com>
2022-06-27 14:08:59 +0300

    Update ext_helper.py

--- unzipper/modules/ext_script/ext_helper.py
+++ unzipper/modules/ext_script/ext_helper.py
@@ -56,10 +56,10 @@ async def make_keyboard(paths, user_id, chat_id):
     i_kbd = InlineKeyboard(row_width=1)
     data = []
     data.append(
-        InlineKeyboardButton(f"تحميل الكل | Upload All ♻️", f"ext_a|{user_id}|{chat_id}")
+        InlineKeyboardButton(f"Upload All ♻️", f"ext_a|{user_id}|{chat_id}")
     )
     data.append(
-        InlineKeyboardButton("إلغاء | Cancel ❌", "cancel_dis")
+        InlineKeyboardButton("Cancel ❌", "cancel_dis")
     )
     for file in paths:
         data.append(
